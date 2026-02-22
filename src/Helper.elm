3b4266c1d85ff26e621617b2844fc6220d9f6884
up230085-debug <up230085@alumnos.upa.edu.mx>
2026-02-22 17:44:40 -0600

    Tarea 4

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,11 +1,51 @@
-module Helper exposing (..)
+module Helper exposing (joinWords, isUpperChars, evalChars, headers, hyperlink)
 
-import Html
-import Html.Attributes
+import Char
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
+import Html.Attributes exposing (href)
+
+
+-- joinWords
+
+joinWords : String -> String -> String
+joinWords str1 str2 =
+    str1 ++ str2
 
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
---main : Html.Html msg
---main =
---    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+-- isUpperChars
+
+isUpperChars : List Char -> List Bool
+isUpperChars chars =
+    List.map Char.isUpper chars
+
+
+
+-- evalChars
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars chars fn =
+    List.map fn chars
+
+
+
+-- headers
+
+headers : String -> Html msg
+headers content =
+    div []
+        [ h1 [] [ text content ]
+        , h2 [] [ text content ]
+        , h3 [] [ text content ]
+        , h4 [] [ text content ]
+        , h5 [] [ text content ]
+        , h6 [] [ text content ]
+        ]
+
+
+
+-- hyperlink
+
+hyperlink : String -> String -> Html msg
+hyperlink url label =
+    a [ href url ] [ text label ]
